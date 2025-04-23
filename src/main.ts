5ead7f263e293fe0ea7e0007a8a683920c8c3899
Roy <satriaroy70@gmail.com>
2025-04-23 21:02:18 +0700

    fix(jest): update jest config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { WINSTON_MODULE_NEST_PROVIDER, WinstonModule } from 'nest-winston';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { winstonOptions } from './logger/logger.config';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        logger: WinstonModule.createLogger(winstonOptions),
+    });
 
     const winstonLogger = app.get(WINSTON_MODULE_NEST_PROVIDER);
     app.useLogger(winstonLogger);
@@ -16,6 +19,7 @@ async function bootstrap() {
 
     const configService = app.get(ConfigService);
     const port = configService.get<number>('PORT');
+
     await app.listen(port ?? 3000);
 }
 bootstrap();
--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UsersController } from './user.controller';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
-describe('UsersController', () => {
-    let controller: UsersController;
+describe('UserController', () => {
+    let controller: UserController;
+
+    const mockUserService = {
+        postUser: jest.fn((dto) => {
+            return {
+                id: 1,
+                username: dto.username,
+                email: dto.email,
+            };
+        }),
+    };
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            controllers: [UsersController],
-        }).compile();
+            controllers: [UserController],
+            providers: [UserService],
+        })
+            .overrideProvider(UserService)
+            .useValue(mockUserService)
+            .compile();
 
-        controller = module.get<UsersController>(UsersController);
+        controller = module.get<UserController>(UserController);
     });
 
     it('should be defined', () => {
         expect(controller).toBeDefined();
+    });
+
+    it('should be able to create new user', async () => {
+        const requestBody = {
+            username: 'userTest1',
+            email: 'user-test-01@gmail.com',
+            password: 'password01',
+            confirmPassword: 'password01',
+        };
+        await expect(controller.postUser(requestBody)).resolves.toEqual({
+            data: {
+                id: expect.any(Number),
+                username: requestBody.username,
+                email: requestBody.email,
+            },
+            message: null,
+        });
     });
 });
